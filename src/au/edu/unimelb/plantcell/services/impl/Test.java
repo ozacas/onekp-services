cc164d6fc5bf545cea951645f2a45c4736c5fa80
pcbrc-enquiries@unimelb.edu.au <pcbrc-enquiries@unimelb.edu.au@6c90420c-eadf-0bee-7b0b-1167c6c39b56>
2014-11-10 03:14:40 +0000

    added some test drivers skeletons and added MAAB REST service for downloading scripts

--- src/au/edu/unimelb/plantcell/services/impl/Test.java
+++ src/au/edu/unimelb/plantcell/services/impl/Test.java
@@ -1,5 +1,6 @@
 package au.edu.unimelb.plantcell.services.impl;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -12,8 +13,42 @@ import javax.ws.rs.core.MediaType;
 public class Test {
 
 	@GET
-	@Path("hi")
-	public String getTest() {
-		return "Hi!";
+	@Path("all")
+	@RolesAllowed("1kp_admin")
+	public String doAllTests() {
+		try {
+			runTests();
+			return "PASS";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "FAIL: "+e.getMessage();
+		}
+	}
+	
+	private void runTests() throws Exception {
+		run25Tests();
+		run25STests();
+		run39Tests();
+		run49Tests();
+		run59Tests();
+		run69Tests();
+	}
+
+	private void run69Tests() {
+	}
+
+	private void run59Tests() {
+	}
+
+	private void run49Tests() {
+	}
+
+	private void run39Tests() {
+	}
+
+	private void run25STests() {
+	}
+
+	private void run25Tests() {
 	}
 }
